7cd6b982e7a7b1cb77e2cab1ca196b703c45e727
anyefremova <anna.efremova.02@yandex.ru>
2023-07-17 01:49:42 +0500

    task 2

--- task 2/index.js
+++ task 2/index.js
@@ -1,3 +1,4 @@
+//1 задание
 const grades = [];
 for (i = 0; i < 12; i++) {
     const randomGrades = Math.floor(Math.random() * 100) + 1;
@@ -5,3 +6,24 @@ for (i = 0; i < 12; i++) {
     console.log(randomGrades);
 }
 
+//2 задание
+const gradesAverage = grades.reduce((partialSum, grade) => partialSum + grade, 0);
+console.log(gradesAverage);
+
+//3 задание
+const maxGrade = Math.max(...grades);
+console.log(maxGrade);
+
+//4 задание
+const minGrade = Math.min(...grades);
+console.log(minGrade);
+
+//5 задание
+const positiveGrade = grades.filter(el => el >= 60);
+console.log(positiveGrade.length);
+
+//6 задание
+const badGrade = grades.filter(el => el < 60);
+console.log(badGrade.length);
+
+//7 задание
